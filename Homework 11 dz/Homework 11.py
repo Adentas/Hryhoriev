9b4a2e75558543ae0c31070d50902678f4399fc8
Adentas <127105025+Adentas@users.noreply.github.com>
2023-06-27 15:19:32 +0300

    Update Homework 11.py

--- Homework 11 dz/Homework 11.py
+++ Homework 11 dz/Homework 11.py
@@ -5,87 +5,97 @@ import re
 
 class Field:
     def __init__(self, value=None):
+        self._value = None
         self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        self._value = new_value
 
 
 class Name(Field):
     pass
 
+# Клас Phone має статичний метод для перевірки валідності номера телефону
+class Phone:
+    def __init__(self, value):
+        self.value = value
 
-class Phone(Field):
-    pass
+    @staticmethod
+    def is_valid_phone(phone):
+        # Перевірка валідності формату номера телефону
+        # Наприклад, формат: +380XXXXXXXXX
+        if phone.startswith("+380") and len(phone) == 13:
+            return True
+        return False
 
 
-class Birthday(Field):
-    def __init__(self, value=None):
-        super().__init__(value)
-        self.validate_birthday()
+class Birthday:
+    def __init__(self, value):
+        self.value = value
 
-    def validate_birthday(self):
-        if self.value:
-            try:
-                date_parts = self.value.split(".")
-                if len(date_parts) == 3:
-                    day = int(date_parts[0])
-                    month = int(date_parts[1])
-                    year = int(date_parts[2])
-                    date(year, month, day)
-                else:
-                    self.value = None
-            except ValueError:
-                self.value = None
+    @staticmethod
+    def is_valid_birthday(birthday):
+        # Перевірка валідності формату дати народження
+        # Наприклад, формат: DD.MM.YYYY
+        parts = birthday.split(".")
+        if len(parts) == 3:
+            day, month, year = parts
+            if len(day) == 2 and len(month) == 2 and len(year) == 4:
+                return True
+        return False
 
 
 class Record:
-    def __init__(self, name, phone=None, birthday=None):
-        self.name = Name(name)
-        self.phone = Phone(phone)
-        self.birthday = Birthday(birthday)
+    def __init__(self, name):
+        self.name = name
+        self.phones = []
+        self.birthday = None
 
     def add_phone(self, phone):
-        self.phone = Phone(phone)
-
-    def remove_phone(self):
-        self.phone = None
+        self.phones.append(Phone(phone))
 
     def edit_phone(self, new_phone):
-        self.phone = Phone(new_phone)
+        if self.phones:
+            self.phones[0] = Phone(new_phone)
 
     def add_birthday(self, birthday):
         self.birthday = Birthday(birthday)
 
-    def __str__(self):
-        record_str = f"Name: {self.name.value}\n"
-        if self.phone.value:
-            record_str += f"Phone: {self.phone.value}\n"
-        if self.birthday.value:
-            record_str += f"Birthday: {self.birthday.value}\n"
-        return record_str
-
     def days_to_birthday(self):
-        if self.birthday.value:
+        if self.birthday:
             today = date.today()
-            birthday_date_parts = self.birthday.value.split(".")
-            birthday_day = int(birthday_date_parts[0])
-            birthday_month = int(birthday_date_parts[1])
-            next_birthday = date(today.year, birthday_month, birthday_day)
+            next_birthday = date(today.year, self.birthday.value.month, self.birthday.value.day)
             if next_birthday < today:
-                next_birthday = date(today.year + 1, birthday_month, birthday_day)
-            days_left = (next_birthday - today).days
-            return days_left
+                next_birthday = date(today.year + 1, self.birthday.value.month, self.birthday.value.day)
+            return (next_birthday - today).days
+        return None
+
+    def __str__(self):
+        phones_str = "\n".join(phone.value for phone in self.phones)
+        if self.birthday:
+            return f"Name: {self.name}\nPhones:\n{phones_str}\nBirthday: {self.birthday.value}"
         else:
-            return None
+            return f"Name: {self.name}\nPhones:\n{phones_str}"
 
 
-class AddressBook(UserDict):
-    def __init__(self, n=5):
-        super().__init__()
-        self.n = n
+class AddressBook:
+    def __init__(self):
+        self.data = {}
+
+    def __contains__(self, name):
+        return name in self.data
+
+    def __iter__(self):
+        return iter(self.data.values())
 
     def iterator(self):
-        records = list(self.data.values())
-        for i in range(0, len(records), self.n):
-            yield records[i:i + self.n]
+        for i in range(0, len(self.data), self.n):
+            yield list(self.data.values())[i:i + self.n]
 
     def add_record(self, record):
         self.data[record.name.value] = record
@@ -110,90 +120,62 @@ class AddressBook(UserDict):
 contacts = AddressBook()
 
 
+# Функція для додавання контакту до адресної книги
 def add_contact_to_addressbook(name, phone):
     if name in contacts:
         record = contacts[name]
         record.add_phone(phone)
-        return "Phone number added successfully"
+        return "Номер телефону додано успішно"
     else:
         record = Record(name)
         record.add_phone(phone)
         contacts.add_record(record)
-        return "Contact added successfully"
+        return "Контакт додано успішно"
 
 
-def add_contact_to_addressbook_with_birthday(name, phone, birthday):
+# Функція для додавання дати народження до контакту в адресній книзі
+def add_birthday_to_addressbook(name, birthday):
     if name in contacts:
         record = contacts[name]
-        record.add_phone(phone)
-        record.add_birthday(birthday)
-        return "Номер телефону та дата народження успішно додані"
+        if Birthday.is_valid_birthday(birthday):
+            record.add_birthday(birthday)
+            return "Дата народження додана успішно"
+        else:
+            return "Некоректний формат дати народження. Використовуйте формат DD.MM.YYYY"
     else:
-        record = Record(name)
-        record.add_phone(phone)
-        record.add_birthday(birthday)
-        contacts.add_record(record)
-        return "Контакт успішно доданий"
+        return "Контакт не знайдено"
 
 
+# Функція для зміни номера телефону контакту в адресній книзі
 def change_phone_in_addressbook(name, new_phone):
     if name in contacts:
-        record = contacts[name]
-        record.edit_phone(new_phone)
-        return "Phone number updated successfully"
+        if Phone.is_valid_phone(new_phone):
+            record = contacts[name]
+            record.edit_phone(new_phone)
+            return "Номер телефону змінено успішно"
+        else:
+            return "Некоректний формат номера телефону. Використовуйте формат +380XXXXXXXXX"
     else:
-        return "Contact not found"
+        return "Контакт не знайдено"
 
 
+# Функція для отримання номера телефону за ім'ям контакту
 def get_phone_from_addressbook(name):
     if name in contacts:
         record = contacts[name]
-        return str(record)
+        if record.phones:
+            return record.phones[0].value
+        else:
+            return "У контакта немає номера телефону"
     else:
-        return "Contact not found"
-
-
-def show_all_contacts():
-    contact_list = []
-    if contacts:
-        for chunk in contacts.iterator():
-            contact_list.append("\n".join(str(record) for record in chunk))
-    return contact_list
-
-
-def is_valid_phone(phone):
-    # Шаблон для перевірки номера телефону: +380 та 9 будь-яких цифр
-    pattern = r'^\+380\d{9}$'
-    return re.match(pattern, phone) is not None
-
-
-def is_valid_birthday(birthday):
-    # Перевірка формату дати (ДД.ММ.РРРР)
-    pattern = r"\d{2}\.\d{2}\.\d{4}"
-    if not re.match(pattern, birthday):
-        return False
-
-    # Розділення дати на день, місяць та рік
-    day, month, year = birthday.split(".")
-
-    # Перевірка коректності дня
-    if not (1 <= int(day) <= 31):
-        return False
-
-    # Перевірка коректності місяця
-    if not (1 <= int(month) <= 12):
-        return False
-
-    # Перевірка коректності року
-    if not (1900 <= int(year) <= 2100):
-        return False
-
-    return True
+        return "Контакт не знайдено"
 
 
 def main():
+    print("Привіт! Я твій помічник у керуванні адресною книгою.")
+
     while True:
-        command = input("Enter a command: ")
+        command = input("Введіть команду: ")
         output = process_command(command)
         print(output)
         if output == "Good bye!":
@@ -201,84 +183,55 @@ def main():
 
 
 def process_command(command):
-    command = command.lower()
+    if command.lower() == "hello":
+        return "Привіт! Я твій помічник у керуванні адресною книгою."
 
-    if command == "hello":
-        return "How can I help you?"
-
-    if command == "show all":
-        return "\n".join(show_all_contacts())
+    elif command.lower() == "show all":
+        if contacts:
+            return "\n\n".join(str(record) for record in contacts)
+        else:
+            return "Адресна книга порожня"
 
     elif command.startswith("add"):
-        args = command[4:].split()
-        if len(args) == 2:
-            name, phone = args
-            if not is_valid_phone(phone):
-                return "Некоректний номер телефону. Будь ласка, введіть коректний номер у форматі +380XXXXXXXXX."
-            return add_contact_to_addressbook(name, phone)
-        elif len(args) == 3:
-            name, phone, birthday = args
-            if not is_valid_phone(phone):
-                return "Некоректний номер телефону. Будь ласка, введіть коректний номер у форматі +380XXXXXXXXX."
-            if not is_valid_birthday(birthday):
-                return "Некоректна дата народження. Будь ласка, введіть коректну дату у форматі ДД.ММ.РРРР."
-            return add_contact_to_addressbook_with_birthday(name, phone, birthday)
+        params = command[4:].strip().split(" ")
+        if len(params) >= 2:
+            name = params[0]
+            phone = params[1]
+            birthday = None
+            if len(params) == 3:
+                birthday = params[2]
+            if Birthday.is_valid_birthday(birthday):
+                return add_birthday_to_addressbook(name, birthday)
+            else:
+                return add_contact_to_addressbook(name, phone)
         else:
-            return "Некоректна команда. Будь ласка, введіть add, ім'я контакту, номер телефону та, за бажанням, дату народження."
+            return "Некоректна команда. Введіть 'add', ім'я контакту та номер телефону."
 
     elif command.startswith("change"):
-        args = command[7:].split()
-        if len(args) == 2:
-            name, new_phone = args
-            if not is_valid_phone(new_phone):
-                return "Некоректний номер телефону. Будь ласка, введіть коректний номер у форматі +380XXXXXXXXX."
-            return change_phone_in_addressbook(name, new_phone)
+        params = command[7:].strip().split(" ")
+        if len(params) == 2:
+            name = params[0]
+            new_phone = params[1]
+            if Phone.is_valid_phone(new_phone):
+                return change_phone_in_addressbook(name, new_phone)
+            else:
+                return "Некоректна команда. Використовуйте формат 'change', ім'я контакту та новий номер телефону."
         else:
-            return "Некоректна команда. Будь ласка, введіть change, ім'я контакту та новий номер телефону."
+            return "Некоректна команда. Використовуйте формат 'change', ім'я контакту та новий номер телефону."
 
     elif command.startswith("phone"):
-        name = command[6:].strip()
-        return get_phone_from_addressbook(name)
-
-    elif command.startswith("remove"):
-        name = command[7:].strip()
-        if name in contacts:
-            contacts.remove_record(name)
-            return "Контакт успішно видалений"
+        params = command[6:].strip().split(" ")
+        if len(params) == 1:
+            name = params[0]
+            return get_phone_from_addressbook(name)
         else:
-            return "Контакт не знайдено"
-
-    elif command.startswith("when birthday"):
-        name = command[14:].strip()
-        if name:
-            if name in contacts:
-                record = contacts[name]
-                days = record.days_to_birthday()
-                if days is not None:
-                    if days == 0:
-                        return "Сьогодні день народження!"
-                    else:
-                        return f"До наступного дня народження залишилося {days} днів."
-                else:
-                    return "У цього контакту не вказана дата народження."
-            else:
-                return "Контакт не знайдено"
-        else:
-            return "Будь ласка, введіть ім'я контакту після команди 'when birthday'."
-        
-    elif command == "help":
-        return "Доступні команди:\n- hello\n- show all\n- add <ім'я> <номер телефону> [дата народження]\n- change <ім'я> <новий номер телефону>\n- phone <ім'я>\n- remove <ім'я>\n- find <параметри пошуку>\n- help\n- exit"
+            return "Некоректна команда. Використовуйте формат 'phone' і ім'я контакту."
 
     elif command.lower() in ["good bye", "close", "exit"]:
         return "Good bye!"
 
     else:
-        return "Некоректна команда. Введіть 'help' для отримання довідки."
-
-
-if __name__ == "__main__":
-    main()
-
+        return "Некоректна команда. Спробуйте ще раз."
 
 
 if __name__ == "__main__":
